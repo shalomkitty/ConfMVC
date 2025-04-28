2af52ad2807cfa4865cf52e118e203052bcb071b
shalomkitty <artemchornomaz@gmail.com>
2025-04-28 23:22:26 +0300

    Added excel export and import

--- ConfInfrastructure/Controllers/ConferencesController.cs
+++ ConfInfrastructure/Controllers/ConferencesController.cs
@@ -8,6 +8,11 @@ using Microsoft.EntityFrameworkCore;
 using ConfDomain.Model;
 using ConfInfrastructure;
 using Microsoft.Extensions.Hosting;
+using ClosedXML.Excel;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
 
 namespace ConfInfrastructure.Controllers
 {
@@ -20,6 +25,125 @@ namespace ConfInfrastructure.Controllers
         {
             _context = context;
             _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ExportToExcel()
+        {
+            var products = await _context.Conferences
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Description = p.Description,
+                    Place = p.Place,
+                    Date = p.Date,
+                    Price = p.Price
+                })
+                .ToListAsync();
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Conferences");
+
+                worksheet.Cell(1, 1).Value = "Id";
+                worksheet.Cell(1, 2).Value = "Title";
+                worksheet.Cell(1, 3).Value = "Description";
+                worksheet.Cell(1, 4).Value = "Place";
+                worksheet.Cell(1, 5).Value = "Date";
+                worksheet.Cell(1, 6).Value = "Price";
+
+                int row = 2;
+                foreach (var item in products)
+                {
+                    worksheet.Cell(row, 1).Value = item.Id;
+                    worksheet.Cell(row, 2).Value = item.Title;
+                    worksheet.Cell(row, 3).Value = item.Description;
+                    worksheet.Cell(row, 4).Value = item.Place;
+                    worksheet.Cell(row, 5).Value = item.Date;
+                    worksheet.Cell(row, 6).Value = item.Price;
+                    row++;
+                }
+
+                worksheet.Columns().AdjustToContents();
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    stream.Position = 0;
+
+                    string fileName = $"ProductsReport_{DateTime.Now:yyyyMMddHHmmss}.xlsx";
+                    return File(stream.ToArray(),
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        fileName);
+                }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Import()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ImportFromExcel(IFormFile excelFile)
+        {
+            if (excelFile == null || excelFile.Length == 0)
+            {
+                TempData["ImportMessage"] = "File isn't selectted or it's empty.";
+                return RedirectToAction("Import");
+            }
+
+            try
+            {
+                using (var stream = new MemoryStream())
+                {
+                    await excelFile.CopyToAsync(stream);
+                    stream.Position = 0;
+
+                    using (var workbook = new ClosedXML.Excel.XLWorkbook(stream))
+                    {
+                        var worksheet = workbook.Worksheet("Confereces");
+                        int row = 2;
+                        while (!worksheet.Cell(row, 1).IsEmpty())
+                        {
+                            var idCell = worksheet.Cell(row, 1).GetValue<string>();
+                            var titleCell = worksheet.Cell(row, 2).GetValue<string>();
+                            var descriptionCell = worksheet.Cell(row, 3).GetValue<string>();
+                            var placeCell = worksheet.Cell(row, 4).GetValue<string>();
+                            var dateCell = worksheet.Cell(row, 5).GetValue<string>();
+                            var priceCell = worksheet.Cell(row, 6).GetValue<string>();
+
+                            int id = 0;
+                            int.TryParse(idCell, out id);
+
+                            var conference = new Conference
+                            {
+                                Id = id,
+                                Title = titleCell?.Trim(),
+                                Description = descriptionCell?.Trim(),
+                                Place = placeCell?.Trim(),
+                                Date = DateTime.TryParse(dateCell, out DateTime date) ? date : DateTime.Now,
+                                Price = int.TryParse(priceCell, out int price) ? price : 0,
+                            };
+                            _context.Conferences.Add(conference);
+
+                            row++;
+                        }
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+                TempData["ImportMessage"] = "Import successful";
+            }
+            catch (Exception ex)
+            {
+                TempData["ImportMessage"] = $"Import error: {ex.Message}";
+            }
+
+            return RedirectToAction("Index");
         }
 
         // GET: Conferences
---
+++ ConfInfrastructure/Views/Conferences/Import.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConfInfrastructure.Views.Conferences
+{
+    public class ImportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
